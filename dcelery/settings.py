8f91481e84a9e8392c022ce56b32ef7a45e5220b
Muhammadrizo04 <muhammadrizomuxtorov389@gmail.com>
2024-07-02 16:02:28 +0500

    add redis

--- dcelery/settings.py
+++ dcelery/settings.py
@@ -105,3 +105,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
